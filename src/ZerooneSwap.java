08d12262f5b488c8b86a6f7818864cd762eb844e
adarsh3092 <jainadarsh3092@gmail.com>
2018-07-01 00:44:13 +0530

    changes in comment this 0,1 and 2 swaping problem

--- src/ZerooneSwap.java
+++ src/ZerooneSwap.java
@@ -1,41 +1,47 @@
-
+/*swap 0,1 or 2 one side of array all 0,1 and 2 comes to
+ * input:: array[0,1,2,0,1,1,2]
+ * output::array[0,0,1,1,2,2]
+ * Time complexity of thsi program should be O(N) and no of iteration should only one.
+ * 
+ * */
 public class ZerooneSwap {
 
 	public static void main(String[] args) {
 		int arr[]= {2,0,2,1,0,2,1,1,0};
 		int l=0;
 		int r=arr.length-1;
-		int k=r;
-		while(l<r&&k>=0) {
-			if(arr[l]!=0&&arr[k]==0)
-			{
-				int t=arr[k];
-				arr[k]=arr[l];				
-				arr[l]=t;
+		int mid=0;
+		while(mid<=r) {
+			switch(arr[mid]) {
+			case 0:
+				swap(arr,l,mid);
 				l++;
+				mid++;
+				break;
+			case 1:
+				mid++;
+				break;
+			case 2:
+				swap(arr,mid,r);
+				r--;
+				break;
 			}
-			if(arr[l]==0)
-				l++;
+		}
 			
-			 if(arr[r]!=2&&arr[k]==2) {
-				 int t=arr[k];
-					arr[k]=arr[r];
-					arr[r]=t;
-					r--;
-					
-			 }
-			 if(arr[r]==2)
-				 r--;
-			 k--;
-			 	}
-		for(int i:arr)
+		
+	
+	for(int i:arr) {
 		System.out.print(i+" ");
 	}
-	static void swap(int m,int n) {
-		
+	}
+	static void swap(int arr[],int i,int j) {
+		int t=arr[i];
+		arr[i]=arr[j];
+		arr[j]=t;
 	}
 		
 }
+
 //2,0,2,1,0,2,1,1,0
 //0,0,2,1,0,2,1,1,2
 //---0,0,2,1,0,1,1,2,2-->00012112

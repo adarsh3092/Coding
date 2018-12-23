acace94896d21b36229a25fa145347eceae674b1
adarsh3092 <jainadarsh3092@gmail.com>
2018-12-23 12:32:49 +0530

    find max trip each of year

--- saprk-learning/src/main/scala/com/apache/spark/learning/core/SparkDriver.scala
+++ saprk-learning/src/main/scala/com/apache/spark/learning/core/SparkDriver.scala
@@ -1,15 +1,35 @@
 package com.apache.spark.learning.core
 
 import org.apache.spark.sql.SparkSession
+import java.sql.Timestamp
+import java.time.Month
+import org.apache.spark.sql.functions.month
+import org.apache.spark.sql.functions.countDistinct
+import org.apache.spark.sql.functions.count
+import org.apache.spark.sql.functions.max
 
-object SparkDriver {
-  def main(str:Array[String]){
-  val spark= SparkSession.builder().master("local[2]").getOrCreate()
-  val broadCastvariible=spark.sparkContext.broadcast("adarsh")
-  println(broadCastvariible.value)
-  
-  //spark.range(1, 12).show()
-    
-    println("Hello scala world!!")
+import org.apache.spark.sql.expressions.Window
+
+
+object UserMaximumTripInEachMonth {
+  def main(str: Array[String]) {
+    val spark = SparkSession.builder().master("local[2]").getOrCreate()
+    import spark.implicits._
+    val seq = Seq(
+      ("uesr1", "trip2", Timestamp.valueOf("2018-12-23 12:01:11")),
+      ("uesr1", "trip1", Timestamp.valueOf("2018-12-10 02:01:11")),
+      ("uesr2", "trip1", Timestamp.valueOf("2018-12-11 21:01:11")),
+      ("uesr2", "trip1", Timestamp.valueOf("2018-11-09 21:01:11")),
+      ("uesr2", "trip2", Timestamp.valueOf("2018-11-01 09:08:00")),
+      ("uesr2", "trip1", Timestamp.valueOf("2018-10-17 19:28:00")),
+      ("uesr1", "trip1", Timestamp.valueOf("2018-10-02 12:08:00")),
+      ("uesr1", "trip1", Timestamp.valueOf("2018-10-12 15:38:00")),
+      ("uesr2", "trip1", Timestamp.valueOf("2018-11-01 09:08:00")))
+      //val 
+    val rdd = spark.sparkContext.parallelize(seq, 8)
+    val tripsDF = rdd.toDF("user", "trip", "datetime")
+   val df  = tripsDF.groupBy(month(tripsDF("datetime")),$"user").agg(count($"user")).orderBy("month(datetime)", "count(user)")
+   val window=Window.partitionBy("month(datetime)")
+   df.withColumn("max_tmp", max("count(user)").over(window)).where($"count(user)"===$"max_tmp").show
   }
 }

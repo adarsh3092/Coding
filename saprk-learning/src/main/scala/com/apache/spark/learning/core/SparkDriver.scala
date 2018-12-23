55cc0a3a7004d8d53376913e536d58502d8e375f
adarsh3092 <jainadarsh3092@gmail.com>
2018-12-23 13:25:46 +0530

    Updated with window functions

--- saprk-learning/src/main/scala/com/apache/spark/learning/core/SparkDriver.scala
+++ saprk-learning/src/main/scala/com/apache/spark/learning/core/SparkDriver.scala
@@ -7,9 +7,9 @@ import org.apache.spark.sql.functions.month
 import org.apache.spark.sql.functions.countDistinct
 import org.apache.spark.sql.functions.count
 import org.apache.spark.sql.functions.max
+import org.apache.spark.sql.functions.row_number
 
 import org.apache.spark.sql.expressions.Window
-
 
 object UserMaximumTripInEachMonth {
   def main(str: Array[String]) {
@@ -25,11 +25,17 @@ object UserMaximumTripInEachMonth {
       ("uesr1", "trip1", Timestamp.valueOf("2018-10-02 12:08:00")),
       ("uesr1", "trip1", Timestamp.valueOf("2018-10-12 15:38:00")),
       ("uesr2", "trip1", Timestamp.valueOf("2018-11-01 09:08:00")))
-      //val 
+    //val
     val rdd = spark.sparkContext.parallelize(seq, 8)
     val tripsDF = rdd.toDF("user", "trip", "datetime")
-   val df  = tripsDF.groupBy(month(tripsDF("datetime")),$"user").agg(count($"user")).orderBy("month(datetime)", "count(user)")
-   val window=Window.partitionBy("month(datetime)")
-   df.withColumn("max_tmp", max("count(user)").over(window)).where($"count(user)"===$"max_tmp").show
+    val df = tripsDF.groupBy(month(tripsDF("datetime")), $"user").agg(count($"user")).orderBy("month(datetime)", "count(user)")
+    val window = Window.partitionBy("month(datetime)")
+    val winForEachMonth = Window.partitionBy(month($"datetime"))
+    val win = Window.partitionBy(month($"datetime"), $"user").orderBy(month($"datetime"))
+    val countTripUsingWindow = tripsDF
+      .withColumn("user_count", row_number().over(win))
+      .withColumn("max_trip", max("user_count").over(winForEachMonth))
+      .where($"user_count" === $"max_trip").show()
+    //df.withColumn("max_tmp", max("count(user)").over(window)).where($"count(user)"===$"max_tmp").show
   }
 }
